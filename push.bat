a3a8a90a274b01b7e7160180995f1e994f72350b
Uzair-debug <mogamaduzair@gmail.com>
2026-07-08 08:29:12 +0200

    Generalize push.bat

--- push.bat
+++ push.bat
@@ -2,8 +2,8 @@
 cd /d D:\Website
 git config user.email "mogamaduzair@gmail.com"
 git config user.name "Uzair-debug"
-git add app.js auth.js car.html index.html profile.html cloudflare-migration.md
-git commit -m "Add user profile pages, owner links, Cloudflare migration guide"
+git add -A
+git commit -m "Update site"
 git push
 echo.
 echo Done! Press any key to close.
